58d49ca0d7b16e1fa1f47cb6630a6e835e64e231
bauGid <fun-man2000@web.de>
2017-07-26 20:39:18 +0200

    parallelised the execution

--- C++friendNumbers/Main.cpp
+++ C++friendNumbers/Main.cpp
@@ -1,35 +1,55 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <thread>
+#include <mutex>
+#include <algorithm>
+#define CORE_COUNT 16
 
 struct pair {
 	unsigned long long val1;
 	unsigned long long val2;
+
+	bool operator<(pair other) {
+		return (val1 < other.val1);
+	}
 };
 
 unsigned long long sumDividers(const unsigned long long& n) {
 	unsigned long long sum = 1;
 	unsigned long long root =sqrtl(n);
-	for (unsigned long long i = 2; i <= root; i++) {
+	for (unsigned long long i = 2; i < root; i++) {
 		if (n%i==0) {
-					sum += i;
-					if (i*i != n) {
-						sum += n / i;
-					}
+			sum += i;
+			sum += n / i;
 		}
+	}
+	if (root*root == n) {
+		sum += root;
 	}
 	return sum;
 }
 
 std::vector<pair> genFriendNumbsToMax(const unsigned long long& max){
 	std::vector<pair> out;
-	unsigned long long dividerSum;
-	for (unsigned long long i = 2; i <= max; i++) {
-		dividerSum = sumDividers(i);
-		if (i < dividerSum&&i == sumDividers(dividerSum)) {
-			out.push_back({ i,dividerSum });
-		}
+	std::mutex lock;
+	std::thread threads[CORE_COUNT];
+	for (int i = 0; i < CORE_COUNT; i++) {
+		threads[i] = std::thread([&out,&lock,min= i == 0 ? 2 : max / CORE_COUNT * i,threadMax = i == CORE_COUNT - 1 ? max : max / CORE_COUNT * (i + 1) - 1]{
+			for (unsigned long long i = min; i <= threadMax; i++) {
+				unsigned long long dividerSum = sumDividers(i);
+				if (i < dividerSum&&i == sumDividers(dividerSum)) {
+					lock.lock();
+					out.push_back({ i,dividerSum });
+					lock.unlock();
+				}
+			}
+		});
 	}
+
+for (int i = 0; i < CORE_COUNT; i++) {
+		threads[i].join();
+}
 	return out;
 }
 
@@ -44,10 +64,14 @@ int main() {
 			std::cout << "Maximum value must be greater than one!\n";
 		}
 	} while (error);
+	std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
 	std::vector<pair> friends = genFriendNumbsToMax(max);
+	std::chrono::high_resolution_clock::time_point stop = std::chrono::high_resolution_clock::now();
+	std::sort(friends.begin(), friends.end());
 	for (size_t i = 0; i < friends.size(); i++) {
 		std::cout << friends[i].val1 << ", " << friends[i].val2 << "\n";
 	}
+	std::cout << "Time: " << std::chrono::duration_cast<std::chrono::seconds>(stop - start).count() << "S\n";
 	std::cin.ignore();
 	std::cin.get();
 }
